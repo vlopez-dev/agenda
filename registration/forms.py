26c59b28abd90d6ed2326c3c774c736ad98bc191
Victor <victorl_222@hotmail.com>
2022-09-01 17:51:42 -0300

    email validation

--- registration/forms.py
+++ registration/forms.py
@@ -1,4 +1,5 @@
 from dataclasses import field
+import email
 from pyexpat import model
 from django import forms
 from django.contrib.auth.models import User
@@ -19,7 +20,11 @@ class UserCreationFormWithEmail(UserCreationForm):
         model=User
         fields=("username","email","password1","password1")
 
-        
+    def clean_email(self):
+        email=self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('El email ya esta registrado, prueba con otro')
+        return email
         
         
     def __init__(self, *args, **kwargs):
