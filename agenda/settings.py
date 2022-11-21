c30550410dd94f7dd276830f04aaa0faa18e9290
victor <victorl_222@hotmail.com>
2022-11-21 11:59:47 -0300

    setting test static url

--- agenda/settings.py
+++ agenda/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 import json
 
@@ -134,8 +135,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
 
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 SWEETIFY_SWEETALERT_LIBRARY = "sweetalert2"
